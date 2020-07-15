a9281df4a28c8dac4ff53dd3543cebbec0439dff
Suraj0712 <sssapkal@wpi.edu>
2020-07-15 11:20:49 -0400

    Updated the mean and covariance matrix for the radardata

---
+++ quiz/q10_predict_mean_and_covariance _for_radar/src/main.cpp
@@ -0,0 +1,20 @@
+#include "/home/sur/SFND_5_Unscented_Kalman_Filter_Highway_Project/Eigen/Dense"
+#include "ukf.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+int main() {
+
+  // Create a UKF instance
+  UKF ukf;
+
+  /**
+   * Programming assignment calls
+   */  
+  VectorXd z_out = VectorXd(3);
+  MatrixXd S_out = MatrixXd(3, 3);
+  ukf.PredictRadarMeasurement(&z_out, &S_out);
+
+  return 0;
+}
