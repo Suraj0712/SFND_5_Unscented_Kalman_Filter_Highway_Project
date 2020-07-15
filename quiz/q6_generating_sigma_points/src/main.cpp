b68fafffb351db2a3b87e7c1d23e91609438e95a
Suraj0712 <sssapkal@wpi.edu>
2020-07-15 10:59:03 -0400

    Added results for sigma point generation quiz

---
+++ quiz/q6_generating_sigma_points/src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "/home/sur/SFND_5_Unscented_Kalman_Filter_Highway_Project/Eigen/Dense"
+#include "ukf.h"
+
+using Eigen::MatrixXd;
+
+int main() {
+
+  // Create a UKF instance
+  UKF ukf;
+
+  /**
+   * Programming assignment calls
+   */
+  MatrixXd Xsig = MatrixXd(5, 11);
+  ukf.GenerateSigmaPoints(&Xsig);
+
+  // print result
+  std::cout << "Xsig = " << std::endl << Xsig << std::endl;
+
+  return 0;
+}
