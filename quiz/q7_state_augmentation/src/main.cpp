34d8606ed3f1ff822b094029c237be51b6d86faf
Suraj0712 <sssapkal@wpi.edu>
2020-07-15 11:06:06 -0400

    Added results for state augumentation

---
+++ quiz/q7_state_augmentation/src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "/home/sur/SFND_5_Unscented_Kalman_Filter_Highway_Project/Eigen/Dense"
+#include "ukf.h"
+
+using Eigen::MatrixXd;
+
+int main() {
+
+  // Create a UKF instance
+  UKF ukf;
+
+  /**
+   * Programming assignment calls
+   */
+  MatrixXd Xsig_aug = MatrixXd(7, 15);
+  ukf.AugmentedSigmaPoints(&Xsig_aug);
+
+  return 0;
+}
